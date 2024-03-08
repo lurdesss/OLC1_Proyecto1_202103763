3a94e0fb093a1672430148151b2ce77d38fcb08e
Lourdes Manuel <3168761941503@ingenieria.usac.edu.gt>
2024-03-08 11:44:12 -0600

    se inicia el proyecto 1

---
+++ src/main/java/vista/Root.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+
+/**
+ *
+ * @author lurde
+ */
+public class Root extends JFrame implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
